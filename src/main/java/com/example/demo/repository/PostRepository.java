2fac47848ea548f92533b64193dba950b67183e5
gebustoc <ge.bustosc@duocuc.cl>
2025-11-13 20:14:01 -0300

    swagger (easier to spam stuff as test fodder)

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Post;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.model.Post;
 
 
 public interface PostRepository extends JpaRepository<Post, Long> {
@@ -13,13 +13,13 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     public Post findByPostID(Long postID);
 
-    @Query(value = "SELECT * FROM post_data where userid = ?1 ORDER BY postid DESC",nativeQuery = true)
+    @Query(value = "SELECT * FROM post_data where posted_by_userid = ?1 ORDER BY postid DESC",nativeQuery = true)
     public Page<Post> findByUserID(Long userID,Pageable pageable);
 
-    @Query(value = "SELECT FLOOR(COUNT(postid)/5) FROM post_data where userid = ?1 ORDER BY postid DESC",nativeQuery = true)
+    @Query(value = "SELECT FLOOR(COUNT(postid)/10) FROM post_data where posted_by_userid = ?1 GROUP BY postid ORDER BY postid DESC",nativeQuery = true)
     public int getUserPages(Long userID);
 
-    @Query(value = "SELECT FLOOR(COUNT(postid)/5) FROM post_data ORDER BY postid DESC",nativeQuery = true)
+    @Query(value = "SELECT FLOOR(COUNT(postid)/10) FROM post_data ORDER BY postid DESC",nativeQuery = true)
     public int getOverallPages();
 
 
